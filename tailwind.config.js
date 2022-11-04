6fd43000a830d85242c996b960f20875cd3743a5
Dj2035 <dk.jude@yahoo.com>
2022-11-04 18:18:50 +0100

    Updates

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(223, 12%, 13%)',
+        darkPaleBlue: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+        input: '#2A2A35',
+        inputBorder: '#565666',
+        selectedText: '#A3A3FF',
+        nav: '#404053',
+        secondary: '#9191A4',
+        linkHover: '#001eff'
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      },
+      padding: {
+        '1/3': '33.33333%',
+        '2/3': '66.66667%'
+      },
+    }
+  },
+  plugins: [],
+}
